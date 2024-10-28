427367683f1dbe1bfd36edd1d750868d6a3c3e97
hoangbui244 <94625568+hoangbui244@users.noreply.github.com>
2024-10-28 10:20:28 +0700

    + Fix scene trans

--- Assets/Scripts/Managers/MainUIMananger.cs
+++ Assets/Scripts/Managers/MainUIMananger.cs
@@ -11,24 +11,16 @@ public class MainUIMananger : Singleton<MainUIMananger>
     private static SceneTransition Scene => Instance?.GetComponent<SceneTransition>();
     public bool PopupOpened;
     public int LevelTypeToLoad;
-    public Animator Anim;
-    [SerializeField] private GameObject _sceneTrans;
-    
-    // [SerializeField] private RectTransform _circle;
-    // [SerializeField] private RectTransform _dog;
-    // [SerializeField] private RectTransform _circleChild;
-    
-    // private Vector3 _circleChildInitialScale;
-    // private Vector3 _inverseScale;
-    // private Vector3 _cirScale = new Vector3(30f, 30f, 1);
-    // private Vector3 _dogScale = new Vector3(0.8f, 0.8f, 1);
-    // private Vector3 _cirEndScale = new Vector3(0.01f, 0.01f, 1);
-    // private Vector3 _cirEndChildScale = new Vector3(100f, 100f, 1);
-    
+    [SerializeField] private GameObject _mask;
+    [SerializeField] private GameObject _dog;
+    [SerializeField] private float _time;
+    private Vector3 _maskScaleEnd = new Vector3(25, 25, 1);
+    private Vector3 _dogScaleEnd = new Vector3(0.8f, 0.8f, 1);
+
     #endregion
 
     #region =========================== UNITY CORES ===========================
-    
+
     private void OnEnable()
     {
         PopupOpened = false;
@@ -45,39 +37,19 @@ public class MainUIMananger : Singleton<MainUIMananger>
 
     public void SceneEnd()
     {
-        Anim.Play("SceneEnd");
-        // _sceneTrans.SetActive(true);
-        //
-        // _circle.DOScale(_cirEndScale, 0.6f);
-        // _circleChild.DOScale(_cirEndChildScale, 0.6f);
-        // _dog.DOScale(_dogScale, 0.8f);
+        _mask.transform.DOScale(0, _time).SetEase(Ease.Linear);
+        _dog.transform.DOScale(_dogScaleEnd, _time * 0.6f).SetEase(Ease.Linear);
     }
 
     public void SceneStart()
     {
-        Anim.Play("SceneStart");
+        _dog.transform.DOScale(0, _time * 0.6f).SetEase(Ease.Linear);
+        _mask.transform.DOScale(_maskScaleEnd, _time).SetEase(Ease.Linear).OnComplete(() =>
+        {
+            GameManager.Instance.GamePause(false);
+            Debug.LogError(GameManager.Instance.GameState);
+        });
     }
 
-    // private IEnumerator Wait()
-    // {
-    //     yield return new WaitForSeconds(0.8f);
-    //     _circleChild.localScale = _cirEndScale;
-    //     _circle.DOScale(_cirScale, 0.8f).OnUpdate(() =>
-    //     {
-    //         Vector3 parentScale = _circle.localScale;
-    //         _inverseScale.x = _circleChildInitialScale.x / parentScale.x;
-    //         _inverseScale.y = _circleChildInitialScale.y / parentScale.y;
-    //         _inverseScale.z = _circleChildInitialScale.z / parentScale.z;
-    //     
-    //         _circleChild.localScale = _inverseScale;
-    //     });
-    //     
-    //     _dog.DOScale(Vector3.zero, 0.6f).OnComplete(() =>
-    //     {
-    //         _sceneTrans.SetActive(false);
-    //     });
-    // }
-    
     #endregion
-
 }
