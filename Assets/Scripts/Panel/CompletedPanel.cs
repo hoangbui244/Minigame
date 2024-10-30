52f0f989addb651e43feffb495760ea27c01f257
hoangbui244 <94625568+hoangbui244@users.noreply.github.com>
2024-10-30 17:07:52 +0700

    + Change UI + VFX

--- Assets/Scripts/Panel/CompletedPanel.cs
+++ Assets/Scripts/Panel/CompletedPanel.cs
@@ -8,16 +8,24 @@ using UnityEngine.UI;
 public class CompletedPanel : MonoBehaviour
 {
     [SerializeField] private GameObject _notiText;
+    [SerializeField] private GameObject _obj1;
+    [SerializeField] private GameObject _obj2;
+    [SerializeField] private Animator _anim;
     
     private void OnEnable()
     {
         MainUIMananger.Instance.PopupOpened = true;
+        _anim.Play("Completed");
         _notiText.SetActive(false);
+        _obj1.SetActive(true);
+        _obj2.SetActive(true);
     }
     
     private void OnDisable()
     {
         MainUIMananger.Instance.PopupOpened = false;
+        _obj1.SetActive(false);
+        _obj2.SetActive(false);
     }
     
     public void ShareCompleted()
---
+++ Assets/Scripts/Panel/ConfettiPanel.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConfettiPanel : MonoBehaviour
+{
+    [SerializeField] private Animator _anim;
+    [SerializeField] private GameObject _obj1;
+    [SerializeField] private GameObject _obj2;
+    
+    private void OnEnable()
+    {
+        _anim.Play("Confetti");
+        _obj1.SetActive(true);
+        _obj2.SetActive(true);
+    }
+    
+    private void OnDisable()
+    {
+        _obj1.SetActive(false);
+        _obj2.SetActive(false);
+    }
+}
